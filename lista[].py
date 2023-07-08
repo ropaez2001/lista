b6c362f429ff46a146a8bbce357a197763728d09
ropaez2001 <ropaez2111@gmail.com>
2023-07-08 17:46:09 -0300

    lista

---
+++ lista[].py
@@ -0,0 +1,12 @@
+
+
+lista=[]
+for i in range(1,36):
+    lista.append(i)
+print(lista)
+
+"""palabra="hola"
+lista=[]
+for i in range(len(palabra)):
+    lista.insert(1, palabra)   
+print(lista)"""
